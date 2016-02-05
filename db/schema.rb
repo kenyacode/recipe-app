f3e78559ba39abbef4983771fdf26a1db9dca0bb
Joshua Munene <kenyadevelop@gmail.com>
2016-02-04 20:04:58 -0800

    Add mini_magick, fog gems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205025231) do
+ActiveRecord::Schema.define(version: 20160205034454) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160205025231) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
 end
