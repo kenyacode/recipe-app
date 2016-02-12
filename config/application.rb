f267f6faf97546316f0f4755d66b09d0ad528082
Joshua Munene <kenyadevelop@gmail.com>
2016-02-11 19:08:30 -0800

    fixing a precompile error

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module RecipeBox
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+		# Rails do not bootstrap the application during asset compilation
+		config.assets.initialize_on_precompile = false
   end
 end
